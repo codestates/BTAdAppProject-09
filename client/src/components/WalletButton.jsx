54bd944902e2a54201bd2ccc2b7dc5d95ded643a
Junhyuk Park <junhyeg08@gmail.com>
2022-10-27 19:41:06 +0900

    Feat: add truffle

--- client/src/components/WalletButton.jsx
+++ client/src/components/WalletButton.jsx
@@ -4,13 +4,14 @@ import { useWeb3React } from "@web3-react/core";
 import { injected } from "../lib/connectors";
 import { isNoEthereumObject } from "../lib/errors";
 import NETWORKS from "../utils/chainId";
+import { useCallback } from "react";
 
 const targetNetwork = NETWORKS[process.env.REACT_APP_TARGET_CHAIN_ID];
 
 const WalletButton = () => {
   const { chainId, account, active, activate, deactivate } = useWeb3React();
 
-  const handleConnect = () => {
+  const handleConnect = useCallback(() => {
     if (active) {
       deactivate();
       return;
@@ -20,7 +21,7 @@ const WalletButton = () => {
       if (isNoEthereumObject(error))
         window.open("https://metamask.io/download.html");
     });
-  };
+  }, [activate, active, deactivate]);
 
   useEffect(() => {
     console.log(chainId);
